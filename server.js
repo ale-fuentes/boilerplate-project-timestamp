53c8f01968b818759a4407b2411bd4ab844398b5
alejandro <ale.fuentes.misc@gmail.com>
2021-07-25 10:07:29 -0300

    refac - create api get date

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,42 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
 
+  //if param are empty, them use current date.
+  if(Object.keys(req.params).length === 0){
+    var dateUnix = Date.now().valueOf();
+    var dateUTC = new Date().toUTCString();
+    res.json({
+      unix: dateUnix,
+      utc: dateUTC
+    });
+  }
+  else{
+
+    const { date } = req.params;
+    const isNumber = !isNaN(parseFloat(date)) && isFinite(date);
+    var dataToParseDate = isNumber ? parseInt(date) : date;
+
+    if(!Date.parse(new Date(dataToParseDate))){
+      res.status(400).json({
+        error: "Invalid Date"
+      });
+    }
+
+    var unix = new Date(dataToParseDate).valueOf();
+    var utc = new Date(dataToParseDate).toUTCString()
+
+    res.json({
+      unix: unix,
+      utc: utc
+    });
+  }
+
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
